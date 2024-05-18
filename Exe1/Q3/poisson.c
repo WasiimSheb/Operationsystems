85c94522d3ee0abf1345a44841c2a2c0c83797f8
shifaa_Khatib <132808634+shifaaKh28@users.noreply.github.com>
2024-05-18 20:24:54 +0300

    Update poisson.c

--- Exe1/Q3/poisson.c
+++ Exe1/Q3/poisson.c
@@ -1,8 +1,16 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include <math.h>
 #include "poisson.h"
 
-double poisson(double lambda, int k) {
-     return expl(-lambda) * powl(lambda, k) / tgamma(k + 1);
+long double poisson_calc(long double lambda, int k) {
+    // Calculate P(X=k) using the Poisson probability mass function formula
+    long double probability = (long double)exp(-lambda) * powl(lambda, k) / factorial(k);
+    return probability;
+}
+
+int factorial(int n) {
+    if (n == 0 || n == 1) {
+        return 1;
+    } else {
+        return n * factorial(n - 1);
+    }
 }
