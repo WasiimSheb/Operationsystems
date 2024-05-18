fc6b2f80881155bf0a744bcd1a1766853f6e2ae9
shifaa_Khatib <132808634+shifaaKh28@users.noreply.github.com>
2024-05-18 20:25:09 +0300

    Update main.c

--- Exe1/Q3/main.c
+++ Exe1/Q3/main.c
@@ -1,13 +1,17 @@
-#include <stdio.h>
 #include "poisson.h"
+#include<stdio.h>
 
-int main(){
-   int lambda[5] = {3, 4, 5, 6, 7};
-   int k[5] = {1, 2, 4, 6, 10};
+int main(int argc, char *argv[]){
 
-   for (int i = 0; i < 5; i++){
-    printf("time: %d, lambda's value: %d, k's value: %d, Poisson value: %lf \n", i, lambda[i], k[i], poisson(lambda[i], k[i]));
-   }
+    int k[] = {1, 10, 2, 3, 3};//array for the k values
+    int lambda[] = {2, 2, 2, 3, 100};// array for the lambda
 
-   return 0;  
+    for (int i = 0; i < 5; i++)
+    {
+        //call the poisson_calc
+        printf("%d): lambda = %d, k = %d, poisson_calc = %Lf\n", (i + 1), 
+        lambda[i], k[i], poisson_calc(lambda[i], k[i]));
+    }
+    
+    return 0;
 }
